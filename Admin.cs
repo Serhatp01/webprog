a4ca5c4385dd6620c71bfdd1b15d6e1ec83b5847
Serhatp01 <168634981+Serhatp01@users.noreply.github.com>
2025-12-13 18:56:11 +0300

    Add files via upload

---
+++ Admin.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdevDeneme1.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string KullaniciAdi { get; set; }
+
+        [Required]
+        public string Sifre { get; set; }
+    }
+}
